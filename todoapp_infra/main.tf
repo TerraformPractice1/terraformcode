d0ccf3a97924b39c170ce835acd6d2cbd2ef8ca1
Sameer Shaikh <sameer291@yahoo.co.in>
2025-08-11 09:39:21 +0530

    added new RG

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,6 +4,12 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group_merge" {
+  source                  = "../Module/azurerm_resource_group"
+  resource_group_name     = "todo_app_merge"
+  resource_group_location = "centralindia"
+}
+
 # module "virtual_network_location" {
 #   depends_on = [ module.resource_group ]
 #   source                   = "../Module/azurerm_virtual_network"
